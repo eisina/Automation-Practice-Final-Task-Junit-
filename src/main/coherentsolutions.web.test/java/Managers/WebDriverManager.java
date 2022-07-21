07ef34f7440676ae4c51bfa8ad3ca933a45a852c
cath isina <isinacath378@gmail.com>
2022-07-21 23:35:05 +0400

    Adding Webdriver, LoginPage, LoginTest classes

---
+++ src/main/coherentsolutions.web.test/java/Managers/WebDriverManager.java
@@ -0,0 +1,39 @@
+package Managers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class WebDriverManager {
+
+    private static WebDriver driver;
+
+    public static WebDriver initDriver(String browser) {
+        if (driver == null) {
+            switch (browser) {
+                case "chrome": {
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                    break;
+                }
+                case "firefox": {
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                    break;
+                }
+            }
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
---
+++ src/test/coherentsolutions.web.test/java/LoginTest.java
@@ -0,0 +1,8 @@
+import Managers.WebDriverManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+public class LoginTest {
+
+}
